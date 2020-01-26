3c295a7df76d6c83c22446c0feed256d1e8dc5fd
JDVelasquezO <jdvela158@gmail.com>
2020-01-26 08:45:57 -0600

    routing attributes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,12 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
+    [Route("/user")]
     public class UserController : Controller
     {
+        [Route("/user/data")]
         public IActionResult Index(String name, int age) // Recibimos un parámetro
         {
             String data = name + " " + age; // Usamos el parámetro
             return View("Index", data);
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
         }
     }
 }
